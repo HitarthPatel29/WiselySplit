df438b6073098cd40ae2452dc593517931492b9d
hivip <p2004hitarth@gmail.com>
2025-11-14 17:37:32 -0500

    the backend Deployment initiated

--- WiselySplitServer/src/main/java/ca/mohawkCollege/WiselySplitServer/config/SecurityConfig.java
+++ WiselySplitServer/src/main/java/ca/mohawkCollege/WiselySplitServer/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:*")
+                        .allowedOriginPatterns("http://localhost:5173", "https://wiselysplit.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
